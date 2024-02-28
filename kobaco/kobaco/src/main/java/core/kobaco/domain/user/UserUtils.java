683ac358191fd8794593e15463c4539cca62aa8d
tlarbals824 <simkm0824@naver.com>
2024-02-28 14:47:48 +0900

    feat : 요청 보낸 사용자 정보 조회 유틸 클래스 추가

---
+++ kobaco/kobaco/src/main/java/core/kobaco/domain/user/UserUtils.java
@@ -0,0 +1,18 @@
+package core.kobaco.domain.user;
+
+import core.kobaco.domain.user.service.UserReader;
+import core.kobaco.global.jwt.JwtUtils;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Component;
+
+@Component
+@RequiredArgsConstructor
+public class UserUtils {
+    private final JwtUtils jwtUtils;
+    private final UserReader userReader;
+
+    public User getRequestUser(){
+        final Long userId = jwtUtils.getRequestUserId();
+        return userReader.read(userId);
+    }
+}

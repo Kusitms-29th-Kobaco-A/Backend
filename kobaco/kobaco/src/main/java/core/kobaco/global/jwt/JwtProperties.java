6f576705ac82f2f7a48d5d5a8ee566cf39600b4b
jihyo <wendyjihyo77@sookmyung.ac.kr>
2024-02-29 02:50:20 +0900

    fix: swagger 접속 가능 오류수정

--- kobaco/kobaco/src/main/java/core/kobaco/global/jwt/JwtProperties.java
+++ kobaco/kobaco/src/main/java/core/kobaco/global/jwt/JwtProperties.java
@@ -1,14 +1,26 @@
 package core.kobaco.global.jwt;
 
 import lombok.Getter;
-import lombok.RequiredArgsConstructor;
 import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.stereotype.Component;
 import org.springframework.web.bind.annotation.GetMapping;
 
+
 @Getter
-@RequiredArgsConstructor
 @ConfigurationProperties(prefix = "jwt")
 public class JwtProperties {
-    private final String secret;
-    private final long accessTokenExpirationTime;
+    private String secret;
+    private long accessTokenExpirationTime;
+
+    public JwtProperties() {
+
+    }
+
+    public void setSecret(String secret) {
+        this.secret = secret;
+    }
+
+    public void setAccessTokenExpirationTime(long accessTokenExpirationTime) {
+        this.accessTokenExpirationTime = accessTokenExpirationTime;
+    }
 }

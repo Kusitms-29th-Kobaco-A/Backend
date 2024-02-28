cfe9542b9fd48c8d5e79a8c8fbbc745063adb609
jihyo <wendyjihyo77@sookmyung.ac.kr>
2024-02-28 16:07:43 +0900

    chore: ImageUpload api구현

---
+++ kobaco/kobaco/src/main/java/core/kobaco/ImageUpload/AwsS3Controller.java
@@ -0,0 +1,30 @@
+package core.kobaco.ImageUpload;
+
+import core.kobaco.ApiResponse;
+import io.swagger.annotations.ApiParam;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import java.util.List;
+import io.swagger.annotations.ApiOperation;
+import org.springframework.web.multipart.MultipartFile;
+
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/s3")
+public class AwsS3Controller {
+    private final AwsS3Service awsS3Service;
+
+    @ApiOperation(value = "Amazon S3에 이미지 업로드", notes = "Amazon S3에 이미지 업로드 ")
+    @PostMapping("/image")
+    public ResponseEntity<List<String>> uploadImage(@ApiParam(value="img 파일들(여러 파일 업로드 가능)", required = true) @RequestPart List<MultipartFile> multipartFile) {
+        return ApiResponse.success(awsS3Service.uploadImage(multipartFile));
+    }
+
+    @ApiOperation(value = "Amazon S3에 업로드 된 파일을 삭제", notes = "Amazon S3에 업로드된 이미지 삭제")
+    @DeleteMapping("/image")
+    public ResponseEntity<Void> deleteImage(@ApiParam(value="img 파일 하나 삭제", required = true) @RequestParam String fileName) {
+        awsS3Service.deleteImage(fileName);
+        return ApiResponse.success(null);
+    }
+}

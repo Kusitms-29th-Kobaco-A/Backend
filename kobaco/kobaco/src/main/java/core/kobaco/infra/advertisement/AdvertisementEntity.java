22351816fcca8b9cea1d6023ed9a7784d7fe5f35
jihyo <wendyjihyo77@sookmyung.ac.kr>
2024-02-29 20:39:04 +0900

    fix: 클래스 들여쓰기 수정

--- kobaco/kobaco/src/main/java/core/kobaco/infra/advertisement/AdvertisementEntity.java
+++ kobaco/kobaco/src/main/java/core/kobaco/infra/advertisement/AdvertisementEntity.java
@@ -7,36 +7,37 @@ import lombok.NoArgsConstructor;
 
 import java.time.LocalDateTime;
 
-    @Entity
-    @Table(name = "advertisement")
-    @Getter
-    @NoArgsConstructor(access = AccessLevel.PROTECTED)
-    public class AdvertisementEntity {
-        @Id
-        @GeneratedValue(strategy = GenerationType.IDENTITY)
-        private Long id;
+@Entity
+@Table(name = "advertisement")
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
 
-        private String videoUrl;
-        private String title;
-        private String description;
-        private LocalDateTime createdAt;
-        private String copy;
-        private String copyDetail;
+public class AdvertisementEntity {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
 
-        private AdvertisementEntity(String videoUrl, String title, String description,
+    private String videoUrl;
+    private String title;
+    private String description;
+    private LocalDateTime createdAt;
+    private String copy;
+    private String copyDetail;
+
+    private AdvertisementEntity(String videoUrl, String title, String description,
                                     LocalDateTime createdAt, String copy,String copyDetail) {
-            this.videoUrl = videoUrl;
-            this.title = title;
-            this.description = description;
-            this.createdAt = createdAt;
-            this.copy = copy;
-            this.copyDetail = copyDetail;
-        }
-
-        public static AdvertisementEntity of(String videoUrl, String title, String description,
-                                             LocalDateTime createdAt, String copy,String copyDetail) {
-            return new AdvertisementEntity(videoUrl, title, description, createdAt, copy, copyDetail);
-        }
+        this.videoUrl = videoUrl;
+        this.title = title;
+        this.description = description;
+        this.createdAt = createdAt;
+        this.copy = copy;
+        this.copyDetail = copyDetail;
     }
+
+    public static AdvertisementEntity of(String videoUrl, String title, String description,
+                                             LocalDateTime createdAt, String copy,String copyDetail) {
+        return new AdvertisementEntity(videoUrl, title, description, createdAt, copy, copyDetail);
+    }
+}
 
 

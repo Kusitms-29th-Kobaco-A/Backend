8b43b9da7a32298df3034125fa38adc2b87a51e8
tlarbals824 <simkm0824@naver.com>
2024-02-29 18:51:23 +0900

    chore : deploy.sh 일부 수정

--- .deploy/deploy.sh
+++ .deploy/deploy.sh
@@ -52,10 +52,10 @@ if [ "$application_staus" == "FAIL" ]; then
         $(docker rm -f $green_application_name)
 else
         echo "reload processing"
-        sed -i "s/application_port=.*/application_port=$green_application_port/" .env
         echo $(docker image prune -f)
         echo $(docker rename $blue_application_name $temp_application_name)
         echo $(docker rename $green_application_name $blue_application_name)
+        sed -i "s/application_port=.*/application_port=$green_application_port/" .env
         echo $(docker exec -it nginx-nginx-1 service nginx reload)
         echo $(docker rm -f $temp_application_name)
 fi
